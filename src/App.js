e071c7e423b47e2edede0d411074c413c5e39d37
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-09-05 09:07:48 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // class-based components based project
 import "./App.css";
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
 export default class App extends Component {
   // name = "Abhijeet";
   //  name can be rendered using {this.name}
   render() {
-    return <div>Class-based component</div>;
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
   }
 }
