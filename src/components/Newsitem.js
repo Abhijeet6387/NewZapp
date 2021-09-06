c0d8d17ff30638c2a41eb7604ae693def567d890
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-09-06 10:30:14 +0530

    setting up state using constructor

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let { title, description } = this.props;
+    // setting state as props
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail" className="btn btn-primary btn-sm">
+              Read More
             </a>
           </div>
         </div>
