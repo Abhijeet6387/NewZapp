0e1974101373aa780193e61c67ec24c5e303756e
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-09-11 20:39:19 +0530

    class-based components changed to function for newsitem,spinner and navbar

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 import loading from "./loading.gif";
 // converting class-based to functinal componenent
 // export class Spinner extends Component {
