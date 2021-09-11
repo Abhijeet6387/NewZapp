01a4e975d70e411b183df61bac74ed114062345f
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-09-11 20:37:36 +0530

    class-based components changed to function for newsitem,spinner and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,81 +1,80 @@
 import React, { Component } from "react";
 // import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-export class Navbar extends Component {
-  static propTypes = {};
-
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              <b>NewZapp</b>
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav" style={{ marginLeft: "auto" }}>
-                <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/">
-                    <span className="fa fa-home pl-1 pr-1"></span> Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/business">
-                    <span className="fa fa-briefcase pl-1 pr-1"></span> Business
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/entertainment">
-                    <span className="fa fa-film pl-1 pr-1"></span> Entertainment
-                  </Link>
-                </li>
-                {/* <li className="nav-item">
+// converting class-based to functinal componenent
+// export class Navbar extends Component {
+//   static propTypes = {};
+const Navbar = () => {
+  //   render() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <b>NewZapp</b>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav" style={{ marginLeft: "auto" }}>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">
+                  <span className="fa fa-home pl-1 pr-1"></span> Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  <span className="fa fa-briefcase pl-1 pr-1"></span> Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  <span className="fa fa-film pl-1 pr-1"></span> Entertainment
+                </Link>
+              </li>
+              {/* <li className="nav-item">
                   <Link className="nav-link" to="/general">
                     <span className="fa fa-newspaper-o pl-1 pr-1"></span>{" "}
                     General
                   </Link>
                 </li> */}
-                <li className="nav-item">
-                  <Link className="nav-link" to="/health">
-                    <span className="fa fa-medkit pl-1 pr-1"></span> Health
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/science">
-                    <span className="fa fa-flask pl-1 pr-1"></span> Science
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sports">
-                    <span className="fa fa-futbol-o pl-1 pr-1"></span> Sports
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/technology">
-                    <span className="fa fa-laptop pl-1 pr-1"></span> Technology
-                  </Link>
-                </li>
-              </ul>
-            </div>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  <span className="fa fa-medkit pl-1 pr-1"></span> Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  <span className="fa fa-flask pl-1 pr-1"></span> Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  <span className="fa fa-futbol-o pl-1 pr-1"></span> Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  <span className="fa fa-laptop pl-1 pr-1"></span> Technology
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
+//   }
+// }
 
 export default Navbar;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import loading from "./loading.gif";
-export class Spinner extends Component {
-  render() {
-    return (
-      <div className="text-center">
-        <img src={loading} alt="loading" />
-      </div>
-    );
-  }
-}
+// converting class-based to functinal componenent
+// export class Spinner extends Component {
+// render() {
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img src={loading} alt="loading" />
+    </div>
+  );
+};
+//   }
+// }
 
 export default Spinner;
